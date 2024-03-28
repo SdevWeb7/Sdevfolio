e7cc93531e1ff7e418962390d073e3ee07348ee7
SdevWeb7 <152064117+SdevWeb7@users.noreply.github.com>
2024-03-28 11:55:44 +0100

    commit
    commit

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
             <Toaster position={"bottom-right"} />
          </div>
 
-         <ScrollToTopButton />
+         {/*<ScrollToTopButton />*/}
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
 
      <p>Développeur full-stack passionné</p>
 
+     <div className={'flex gap-8'}>
      <Link
         href="/fullstack"
         className={'btn'}>Full-Stack</Link>
@@ -14,7 +15,7 @@ export default function Home() {
      <Link
         href="/frontend"
         className={'btn'}>Front-End</Link>
-
+     </div>
      <Link
         href={'/a-propos'}
         className={'btn'}>À propos</Link>
